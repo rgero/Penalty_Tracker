3cd457f47817dcabc7bae6b82fa4c71cc5f2241f
Roy Gero <roy.gero.ii@gmail.com>
2015-07-20 15:33:10 -0400

    Changes: -Added the function "getDateFromFile" which scans the file to get the date -Modified the processFile function so that it completely processes all the penalties for the file. Also renamed the function to "processData" since we're not going to be storing the file locally... ideally...
    To do
    -Add More Comments
    -Implement the function to write the master penalty list to a file.
    -Implement the ability to store when the program was last ran.

--- scanning_games.py
+++ scanning_games.py
@@ -2,10 +2,13 @@ from HTMLParser import HTMLParser
 from Teams import *
 from Penalty import *
 
+GameData = []
+
 
 class MyHTMLParser(HTMLParser):
 	'''This generates an Array with all the HTML elements so I can quickly sort through it.'''	
-	#def handle_data(self, data):
+	def handle_data(self, data):
+		GameData.append(data)
 	
 	
 def teamsPlaying(scan):
@@ -24,22 +27,50 @@ def refsInGame(scan):
 	location_of_Referees = scan.lower().find("referees")+len("referees: ")
 	end_Of_Refs = scan[location_of_Referees::].lower().find("<")
 	return  scan[location_of_Referees:location_of_Referees+end_Of_Refs].split(", ")
+	
+def getDateFromFile(scan):
+	'''This is going to be very costly time wise but it's a way to implement it.'''
+	tailOfDate = scan.find("</title>")
+	frontOfDate = scan[tailOfDate:0:-1].find(" - ")-1
+	return scan[tailOfDate-frontOfDate:tailOfDate]
+	
+def processPenalty(penalty):
+	penalty = penalty.decode('unicode_escape').encode('ascii','ignore').split("\n")
+	return penalty[1] #Returning the second index because that is where the actual penalty is stored.
 
-def processFile(file):
-	data = open(file,"r")
-	scan = data.read()
+def processData(scan):
+	MasterPenaltyList = [] #So I can return this to the test framework
 	
 	teams = teamsPlaying(scan)
 	referees = refsInGame(scan)
+	date = getDateFromFile(scan)
 	
+			
+	parser= MyHTMLParser() #Creates an instance of the HTML Parser class
+	parser.feed(scan)
 	
-	
-	
-	
-	
-	#parser = MyHTMLParser()
-	#parser.feed(scan)
-	
-	
-file = "Testing.htm"
-processFile(file)
+	inSection = False
+
+	for i in range(0,len(GameData)):
+		if(GameData[i] == "Penalty Summary" or inSection):
+			inSection = True
+
+			if(GameData[i] in Teams):
+				playersTeam = Teams[GameData[i]]
+				playersName = GameData[i+1]
+				playersPenalty = processPenalty(GameData[i+2])
+				
+				if (playersTeam == teams[0]):
+					location = False
+					opponent = teams[1]
+				else:
+					location = True
+					opponent = teams[0]
+					
+				newPenalty = Penalty(playersName, playersTeam, playersPenalty, location, opponent, date, referees)
+				MasterPenaltyList.append(newPenalty)
+			
+		if("Stats" in GameData[i]):
+			inSection = False
+			
+	return MasterPenaltyList	
