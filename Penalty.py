2f2e7fa87c2cdc7c8cf3210cbf4711802006d2c5
Roy Gero <roy.gero.ii@gmail.com>
2015-07-20 15:30:26 -0400

    Changes to the Penalty Class ------------------------------------------- -Added simple error checking on the the Home/Away status and the referees since I am expecting a boolean for the Home/Away status and an array for the referees. -Renamed some variables to make it a bit easier to use. -Added two functions, "getRefsAsString" and "printEvent". Both return strings
    To Do For this part:
    -Comments!

--- Penalty.py
+++ Penalty.py
@@ -1,32 +1,54 @@
 class Penalty:
-    def __init__(self, player, team, penalty, side, opponent, date, ref1, ref2):
-        self.playerName = player
-        self.playerTeam = team
-        self.penalty = penalty
-        self.side = side
-        self.opponent = opponent
-        self.date = date
-        self.ref1 = ref1
-        self.ref2 = ref2
-        
-    def getPlayer(self):
-        return self.playerName
-    
-    def getTeam(self):
-        return self.playerTeam
-    
-    def getPenalty(self):
-        return self.penalty
-    
-    def getSide(self):
-        return self.side
-    
-    def getOpponent(self):
-        return self.opponent
-    
-    def getDate(self):
-        return self.date
-    
-    def getRefs(self):
-        return [self.ref1,self.ref2]
-    
+	def __init__(self, player, team, penalty, side, opponent, date, referees):
+		self.name = player
+		self.team = team
+		self.penalty = penalty
+		
+		if (type(side) == bool):
+			self.side = side
+		else:
+			raise TypeError("Location unable to be determined")
+			
+		self.opponent = opponent
+		self.date = date
+		
+		if type(referees) is list:
+			self.refs = referees
+		else:
+			raise TypeError("Referees are not passed in as list")
+		
+	def getPlayer(self):
+		return self.name
+	
+	def getTeam(self):
+		return self.team
+	
+	def getPenalty(self):
+		return self.penalty
+	
+	def getSide(self):
+		if (self.side):
+			return "Home"
+		else:
+			return "Away"
+	
+	def getOpponent(self):
+		return self.opponent
+	
+	def getDate(self):
+		return self.date
+	
+	def getRefs(self):
+		return self.refs
+		
+	def getRefsAsString(self):
+		refereeString = ""
+		for i in self.refs:
+			refereeString += i + ", "
+		refereeString = refereeString[0:len(refereeString)-2]
+		return refereeString
+	
+	def printEvent(self):
+		entry = self.name + " | " + self.team + " | " + self.penalty + " | " + self.date + " | " + self.opponent + " | " + self.getSide() + " | " + self.getRefsAsString()
+		return entry
+		
