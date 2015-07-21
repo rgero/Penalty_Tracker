2e4550407345f05c42f20058a67f93f5ba7e55de
Roy Gero <roy.gero.ii@gmail.com>
2015-07-21 14:23:37 -0400

    Changes to Algorithm Check --------------------------------------
    -Added a dictionary component which incremented each penalty for each team. This allowed me to quickly compare my season long results to those on NHL.com. I found that 3 teams had one extra penalty. This lead to the Nashville, Ottawa, and Edmonton lists.
    
    When looking over the results of the three lists, it was determined that the Edmonton list had an out of date file for the 12/31 game. The Ottawa game on 10/16 had a duplicate entry on the page for Mark Borowiecki. The Nashville game on 11/02/2014 had a duplicate entry for Shea Weber.
    
    This to me confirms that my algorithm is able to every penalty that the NHL site lists.
    
    In terms of time, this program was able to execute over 1230 HTML files and store the data in 38 seconds. This is the worst case scenario of processing the data.
    
    TODO:
    - Implement a way for the program to remember when it was last ran and only run through the games that have occurred between.
    - Implement a way to store and retrieve all of the Penalty data. This might mean writing out all of the penalties to a file, then reading them in upon load.
    - Implement a way to search the penalty list based on player, team, infraction, opponent, location and referees.
    
    The last process change will be to have the penalty list run nightly and have the search capabilities happen upon user request.

--- AlgorithmCheck.py
+++ AlgorithmCheck.py
@@ -1,15 +1,52 @@
 import glob, time, os
 from Penalty import *
 from scanning_games import *
+from TeamsCounter import *
 
+MasterMasterList=[]
+teamCheck = []
+
+#Three teams ended up being as +1
+# Nashville, Edmonton and Ottawa
+nashville = []
+edmonton = []
+ottawa = []
 
 startTime = time.time()
-for filename in glob.iglob('GameLogs\*\*.htm'): #This will iterate through every folder and file
+for filename in glob.iglob('C:\\TestingData\\GameLogs\*\*.htm'): #This will iterate through every folder and file
 	pathway = os.path.dirname(os.path.realpath(filename))
-	print filename
 	index_path = pathway.find("GameLogs\\") + len("GameLogs\\") #Figures out the date based on the file structure
 	date = pathway[index_path::]
 	file = open(filename,"r")
 	scan = file.read()
-	dontCare = processData(scan)
+	temporaryStorage = processData(scan)
+	for i in temporaryStorage:
+		MasterMasterList.append(i)
+		TeamCounter[i.getTeam()] += 1
+		if "Nashville" in i.getTeam():
+			nashville.append(i.printEvent())
+		if "Ottawa" in i.getTeam():
+			ottawa.append(i.printEvent())
+		if "Edmonton" in i.getTeam():
+			edmonton.append(i.printEvent())
+		
 print time.time()-startTime
+
+print len(MasterMasterList)
+
+
+
+'''Takes in the dictionaries, creates arrays and sorts them based on the number of infractions, most at the top.'''
+unsorted_data = []
+for l in TeamCounter:
+	unsorted_data.append([l,TeamCounter[l]])
+sorted_data = sorted(unsorted_data, key=lambda x: x[1], reverse=True)
+for i in sorted_data:
+	print i
+	
+#Printing Data on the three teams to figure out what is wrong
+fileName = "Edmonton_Counted.txt"
+file = open(fileName, 'w')
+for j in edmonton:
+	file.write(j + "\n")
+file.close()
