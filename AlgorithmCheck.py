4b158c2004865f850733b2ac402af81b1024020b
Roy Gero <roy.gero.ii@gmail.com>
2015-07-27 13:49:06 -0400

    I removed two lines by accident from commit 1cb6406. Replaced them.

--- AlgorithmCheck.py
+++ AlgorithmCheck.py
@@ -6,7 +6,7 @@ from TeamsCounter import *
 MasterMasterList=[]
 teamCheck = []
 
-'''startTime = time.time()
+startTime = time.time()
 for filename in glob.iglob('C:\\TestingData\\GameLogs\*\*.htm'): #This will iterate through every folder and file
 	pathway = os.path.dirname(os.path.realpath(filename))
 	index_path = pathway.find("GameLogs\\") + len("GameLogs\\") #Figures out the date based on the file structure
@@ -14,6 +14,9 @@ for filename in glob.iglob('C:\\TestingData\\GameLogs\*\*.htm'): #This will iter
 	file = open(filename,"r")
 	scan = file.read()
 	temporaryStorage = processData(scan)
+	for i in temporaryStorage:
+		MasterMasterList.append(i)
+		TeamCounter[i.getTeam()] += 1
 
 		
 print time.time()-startTime
@@ -22,17 +25,17 @@ print len(MasterMasterList)
 
 
 
-''''''Takes in the dictionaries, creates arrays and sorts them based on the number of infractions, most at the top.''''''
+'''Takes in the dictionaries, creates arrays and sorts them based on the number of infractions, most at the top.'''
 unsorted_data = []
 for l in TeamCounter:
 	unsorted_data.append([l,TeamCounter[l]])
 sorted_data = sorted(unsorted_data, key=lambda x: x[1], reverse=True)
 for i in sorted_data:
 	print i
-'''
+
 	
 #HEALTH CHECK
-testingFile = open("Tested.txt","r")
+testingFile = open("TestingDocs\Tested.txt","r")
 PenaltyList = processFromFile(testingFile)
 for i in PenaltyList:
 	print i.printEvent()
