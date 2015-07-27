b87e7b3a99db0c859b7477066538bab91cfedb14
Roy Gero <roy.gero.ii@gmail.com>
2015-07-27 13:24:01 -0400

    Added a test for the "processingFromFile" function. This would need to be moved to a new testing suite when the function itself gets moved.

--- Tester.py
+++ Tester.py
@@ -42,7 +42,28 @@ class TestingSGMethods(unittest.TestCase):
 		
 	def testProcessFile(self):
 		self.assertEqual(len(processData(scan)),4)
-
+		
+	def testingProcessingFromFile(self):
+		procedurallyGenerated = processData(scan)
+		testingFile = open("TestingDocs\ProcessingFromFileTest.txt","r")
+		readingFromFile = processFromFile(testingFile)
+		self.assertEqual(len(procedurallyGenerated), len(readingFromFile))
+		for i in range(0,len(procedurallyGenerated)):
+			pG = procedurallyGenerated[i]
+			fF = readingFromFile[i]
+			self.assertTrue(pG.getPlayer()==fF.getPlayer(), pG.getPlayer() + " : " + fF.getPlayer() + " in entry " + str(i))
+			self.assertTrue(pG.getTeam()==fF.getTeam(), pG.getTeam() + " : " + fF.getTeam()
+				+ " in entry " + str(i))
+			self.assertTrue(pG.getPenalty()==fF.getPenalty(), pG.getPenalty() + " : " + 
+				fF.getPenalty() + " in entry " + str(i))
+			self.assertTrue(pG.getSide()==fF.getSide(), pG.getSide() + " : " + 
+				fF.getSide() + " in entry " + str(i))
+			self.assertTrue(pG.getOpponent()==fF.getOpponent(), pG.getOpponent() + " : " + 
+				fF.getOpponent() + " in entry " + str(i))
+			self.assertTrue(pG.getDate()==fF.getDate(), pG.getDate() + " : " + 
+				fF.getDate() + " in entry " + str(i))
+			self.assertTrue(pG.getRefsAsString()==fF.getRefsAsString(), pG.getRefsAsString() + " : " + fF.getRefsAsString() + " in entry " + str(i))
+	
 
 		
 '''
@@ -51,10 +72,8 @@ class TestingSGMethods(unittest.TestCase):
 os.system("cls")		
 event = Penalty("Roy Gero","Colorado Avalanche","Too Much Man",True,"Calgary Flames","July 8, 2015",["Don","Ron"])
 
-event.printEvent()
-
 #Setting up a dummy file
-file_name = "Testing.htm"
+file_name = "TestingDocs\\Testing.htm"
 file = open(file_name,'r')
 scan = file.read()
 
