bc38229a18529e71a39a8018e28ed9768522a6e0
Roy Gero <roy.gero.ii@gmail.com>
2015-12-18 16:42:14 -0500

    I wrote the DataChecker.py file today. The reason was because I've been manually spot checking the number of penalties this was recording and comparing it to the NHL's website. This process takes usually 15-20 minutes. Now with this process, all I have to do is get the CSV dump from the website, and copy/paste the Table from the NHL into a CSV file and I can run this.
    It will quickly report the difference between my data and the NHL's data, which provides hints as to where the data is coming from (For example, if the Rangers and the Sabres both were missing penalties, and they were the only two teams, then a game between them is missing.)

---
+++ DataChecker/DataChecker.py
@@ -0,0 +1,62 @@
+''' Data Checker
+'''
+
+import sys
+
+def organizeDataDump(file):
+    teamDump = {}
+    with open(file,'r') as F:
+        for line in F:
+            data = line.split(',')[1].replace('"','')
+            if data.lower() != "team name":
+                data = data.split(" ")
+                data = data[len(data)-1]
+                if teamDump.has_key(data):
+                    teamDump[data]+=1
+                else:
+                    teamDump[data]=1
+    return teamDump
+        
+def processNHLData(file):
+    nhlData = {}
+    teamColumn = -1
+    penaltyColumn = -1
+    with open(file,'r') as F:
+        for line in F:
+            if (teamColumn == -1) or (penaltyColumn == -1):
+                data = line.split(',')
+                for col in range(0,len(data)):
+                    if data[col].lower().find("team") != -1:
+                        teamColumn = col
+                    if data[col].lower().find("pen") != -1:
+                        penaltyColumn = col                    
+            else:
+                data = line.split(',')
+                team = data[teamColumn].split(' ')
+                team = team[len(team)-1]
+                penaltyTotal = data[penaltyColumn]
+                nhlData[team] = penaltyTotal
+    return nhlData
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 2:
+        print "You need two additional arguments"
+        print "The first one is the CSV Spreadsheet saved from the NHL Website"
+        print "The second one is the CSV Data Dump from Roymond.net"
+        sys.exit(-1)
+    else:
+        issueTeams = {}
+        nhlFile = sys.argv[1]
+        rFile = sys.argv[2]
+        rData = organizeDataDump(rFile)
+        nhlData = processNHLData(nhlFile)
+        for i in nhlData:
+            if int(nhlData[i]) != int(rData[i]):
+                issueTeams[i] = int(nhlData[i])-int(rData[i])
+        if len(issueTeams) != 0:
+            print "Error! There is a difference between the NHL Data and Roymond Data"
+            print "Team\t:\tDifference"
+            for i in issueTeams:
+                print i + "\t: \t" + str(issueTeams[i])
+        else:
+            print "Everything is Fine"
